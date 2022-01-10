8c7e545c7c1f2c7d8004ceb431fa6d51d5084555
Raphaël Ventura <raphael.ventura@beta.gouv.fr>
2022-01-10 15:40:39 +0100

    Update develop revision to PEP440-compliant format.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = "2022, Signaux Faibles contributors"
 author = "Signaux Faibles contributors"
 
 # The full version, including alpha/beta/rc tags
-release = "dev"
+release = "2022.1.dev1"
 
 
 # -- General configuration ---------------------------------------------------
