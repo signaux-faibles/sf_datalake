7d0c6598a29935486ce861948c74cc6c77bd782b
Raphaël Ventura <raphael.ventura@beta.gouv.fr>
2022-01-12 17:00:28 +0100

    Fix small errors and update exploration functions.

--- src/sf_datalake/exploration.py
+++ src/sf_datalake/exploration.py
@@ -39,94 +39,114 @@ def count_nan_values(
     """
     return df.select(
         [
-            F.count(F.when(F.isnull(c), c)).alias(c)
+            F.count(F.when(F.isnan(c), c)).alias(c)
             for (c, c_type) in df.dtypes
             if c_type not in omit_type
         ]
     )
 
 
-def acounting_time_span(
+def accounting_time_span(
     df: pyspark.sql.DataFrame,
-) -> Tuple[pyspark.sql.DataFrame, pyspark.sql.DataFrame]:
+) -> Tuple[pyspark.sql.Row, pyspark.sql.Row]:
     """Computes global time spans of companies accouting years.
 
     Args:
         df: The input DataFrame. It must have a "date_deb_exercice" and
-          "date_fin_exercice".
+          "date_fin_exercice" columns.
 
     Returns:
         A couple of DataFrame objects, each containing the min and max dates associated
           with the beginning and end of accounting years ("exercice comptable").
 
     """
+    assert {"date_deb_exercice", "date_fin_exercice"} <= set(df.columns)
+
     date_deb_exercice_span = df.select(
-        min("date_deb_exercice"),
-        max("date_deb_exercice"),
+        F.min("date_deb_exercice"),
+        F.max("date_deb_exercice"),
     ).first()
     date_fin_exercice_span = df.select(
-        min("date_fin_exercice"),
-        max("date_fin_exercice"),
+        F.min("date_fin_exercice"),
+        F.max("date_fin_exercice"),
     ).first()
 
     return date_deb_exercice_span, date_fin_exercice_span
 
 
-def accounting_duration(
-    df: pyspark.sql.DataFrame, unit: str = "days"
+def accounting_year_distribution(
+    df: pyspark.sql.DataFrame, unit: str = "days", duration_col: str = "ay_duration"
 ) -> pyspark.sql.DataFrame:
-    """Computes number of days / months in the declared accounting year.
+    """Computes declared accounting year duration distribution.
 
     Args:
         df: The input DataFrame. It must have a "date_deb_exercice" and
-          "date_fin_exercice".
+          "date_fin_exercice" columns.
         unit: The unit measuring accounting year duration. Should be "days" or "months".
+        duration_col: The accounting year duration column name.
 
     Returns:
-        A DataFrame grouping the number of accounting years associated with a given
-          duration in days / months
+        The count of declared accounting years for each given duration.
 
     """
+    assert {"date_deb_exercice", "date_fin_exercice"} <= set(df.columns)
+
+    ayd = accounting_year_duration(df, unit, duration_col)
+    return ayd.groupBy(duration_col).count().orderBy(duration_col)
+
+
+def accounting_year_duration(
+    df: pyspark.sql.DataFrame, unit: str = "days", duration_col: str = "ay_duration"
+) -> pyspark.sql.DataFrame:
+    """Computes declared accounting year duration.
+
+    Args:
+        df: The input DataFrame. It must have a "date_deb_exercice" and
+          "date_fin_exercice" columns.
+        unit: The unit measuring accounting year duration. Should be "days" or "months".
+        duration_col: The accounting year duration column name.
+
+    Returns:
+        A df with a new column of declared accounting years duration.
+
+    """
+    assert {"date_deb_exercice", "date_fin_exercice"} <= set(df.columns)
+
     if unit == "days":
-        avg_duration = df.select(
-            F.datediff(
-                F.to_date(df["date_fin_exercice"]),
-                F.to_date(df["date_deb_exercice"]),
-            ).alias("exercice_datediff")
-        )
+        diff_function = F.datediff
     elif unit == "months":
-        avg_duration = df.select(
-            F.months_between(
-                F.to_date(df["date_fin_exercice"]),
-                F.to_date(df["date_deb_exercice"]),
-            ).alias("exercice_datediff")
-        )
+        diff_function = F.months_between
     else:
         raise ValueError(f"Unknown unit {unit}")
-    return (
-        avg_duration.withColumn(
-            "datediff_floored", F.round("exercice_datediff").cast("int")
-        )
-        .groupBy("datediff_floored")
-        .count()
-        .orderBy("datediff_floored")
+    ayd = df.withColumn(
+        duration_col,
+        F.round(
+            diff_function(
+                F.to_date(df["date_fin_exercice"]),
+                F.to_date(df["date_deb_exercice"]),
+            )
+        ).cast("int"),
     )
+    return ayd
 
 
 def is_centered(df: pyspark.sql.DataFrame, tol: float) -> Tuple[bool, List]:
     """Check if a DataFrame has a `features` column with centered individual variables.
-    `features` column is the result of at least a `VectorAssembler()`.
+
+    The `features` column is the result of at least a `VectorAssembler()`.
 
     Args:
         df : Input DataFrame.
-        tol :  a tolerance for the zero equality test.
+        tol : A tolerance for the zero equality test.
 
     Returns:
-        Tuple[bool, List]: True if variables are centered else False. A list of the
-                            mean of each variable.
+        A couple consisting of:
+          - A bool set to True if variables are centered else False.
+          - A list of each column's mean.
 
     Example:
         is_centered(train_transformed.select(["features"]), tol = 1E-8)
+
     """
     assert "features" in df.columns, "Input DataFrame doesn't have a 'features' column."
 
