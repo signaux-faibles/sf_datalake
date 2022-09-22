f90c4333482cd7f70ac012e79b122e01f11ad49d
Raphaël Ventura <raphael.ventura@beta.gouv.fr>
2022-09-22 18:43:50 +0200

    Fix df instanciation with schemas.

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -2,29 +2,49 @@
 """Test the src/transform.py module.
 """
 
+import datetime as dt
+
 import pytest
+from pyspark.sql import types as T
 
 from sf_datalake.transform import IdentifierNormalizer  # DateParser, SiretToSiren
 
 
 @pytest.fixture
 def siren_padding_df(spark):
-    return spark.createDataFrame(
-        [(524893758, "524893758"), (45378, "000045378"), (54489542, "054489542")],
-        "siren: int, padded_siren: string",
+    schema = T.StructType(
+        [
+            T.StructField("siren", T.IntegerType(), True),
+            T.StructField("padded_siren", T.StringType(), True),
+        ]
     )
+    df = spark.createDataFrame(
+        [(524893758, "524893758"), (45378, "000045378"), (54489542, "054489542")],
+        schema,
+    )
+    return df
 
 
 @pytest.fixture
 def parsed_date_df(spark):
-    return spark.createDataFrame(
-        ["20171130", "2017-11-30"],
-        ["20171229", "2017-12-29"],
-        ["20171229", "2017-12-29"],
-        ["20171229", "2017-12-29"],
-        ["20171031", "2017-10-31"],
-        "raw_date: string, parsed_date: datetime.date",
+    schema = T.StructType(
+        [
+            T.StructField("raw_date", T.StringType(), True),
+            T.StructField("parsed_date", T.DateType(), True),
+        ]
     )
+
+    df = spark.createDataFrame(
+        [
+            ("20171130", dt.date(*(int(s) for s in "2017-11-30".split("-")))),
+            ("20171229", dt.date(*(int(s) for s in "2017-12-29".split("-")))),
+            ("20171229", dt.date(*(int(s) for s in "2017-12-29".split("-")))),
+            ("20171229", dt.date(*(int(s) for s in "2017-12-29".split("-")))),
+            ("20171031", dt.date(*(int(s) for s in "2017-10-31".split("-")))),
+        ],
+        schema,
+    )
+    return df
 
 
 def test_siren_padding(siren_padding_df):
