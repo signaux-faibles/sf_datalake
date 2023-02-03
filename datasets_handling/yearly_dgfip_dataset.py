45e59e254db07f6dd44d242cc970a4882c8686c4
Raphaël Ventura <raphael.ventura@beta.gouv.fr>
2023-02-03 19:07:53 +0100

    Switch input file format to csv in pre-processing scripts.

--- datasets_handling/yearly_dgfip_dataset.py
+++ datasets_handling/yearly_dgfip_dataset.py
@@ -37,7 +37,7 @@ data_paths = {
     "af": path.join(args.input, "etl_decla-declarations_af"),
     "rar_tva": path.join(args.input, "rar_tva_exercice"),
 }
-datasets = sf_datalake.io.load_data(data_paths, file_format="orc")
+datasets = sf_datalake.io.load_data(data_paths, file_format="csv", sep="|")
 
 # Set every column name to lower case (if not already).
 for name, ds in datasets.items():
