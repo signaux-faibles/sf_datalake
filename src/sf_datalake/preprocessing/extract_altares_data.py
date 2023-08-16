1b8e41c51d12457b231f77a47b29cc0e2ede2f38
Raphaël Ventura <raphael.ventura@beta.gouv.fr>
2023-08-16 10:24:32 +0200

    Add new extraction script.

---
+++ src/sf_datalake/preprocessing/extract_altares_data.py
@@ -0,0 +1,62 @@
+"""Altares historicized data preprocessing.
+
+This script parses and extracts data from raw historicized files supplied by Altares. It
+will output a dataset containing the following information:
+- siren
+- periode (date, first day of each month where data is available)
+- paydex
+- fpi30
+- fpi90
+"""
+
+import os
+import sys
+from os import path
+
+import pyspark.sql.functions as F
+
+# isort: off
+sys.path.append(path.join(os.getcwd(), "venv/lib/python3.6/"))
+sys.path.append(path.join(os.getcwd(), "venv/lib/python3.6/site-packages/"))
+# isort: on
+
+# pylint: disable=C0413
+import sf_datalake.io
+import sf_datalake.transform
+import sf_datalake.utils
+
+spark = sf_datalake.utils.get_spark_session()
+
+parser = sf_datalake.io.data_path_parser()
+parser.description = "Extract judgment data"
+parser.add_argument(
+    "--output_format", default="orc", help="Output dataset file format."
+)
+args = parser.parse_args()
+df = spark.read.csv(
+    args.input,
+    sep=";",
+    inferSchema=False,
+    header=True,
+)
+
+df = df.withColumn(
+    "periode",
+    F.to_date(F.date_trunc("month", F.to_date(F.col("DATE_VALEUR"), "dd/MM/yyyy"))),
+)
+# We have to extract from a dumb string format.
+df = df.withColumn(
+    "paydex",
+    F.regexp_extract(
+        F.regexp_replace(F.col("PAYDEX"), r"(^Pas de retard)", "0"), r"(^\d+)", 1
+    ).cast("float"),
+)
+df = df.withColumn("fpi_30", F.col("NOTE100_ALERTEUR_PLUS_30").cast("float"))
+df = df.withColumn("fpi_90", F.col("NOTE100_ALERTEUR_PLUS_90_JOURS").cast("float"))
+df = df.withColumnRenamed("SIREN", "siren")
+
+sf_datalake.io.write_data(
+    df.select(["siren", "periode", "paydex", "fpi_30", "fpi_90"]),
+    args.output,
+    args.output_format,
+)
