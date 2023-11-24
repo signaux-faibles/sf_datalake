34550945a6b73abff1aa5d17ca427c9541ae797e
Hugo Simon <hsimon01@M093-1846016>
2023-11-24 15:51:31 +0100

    remove useless part and add missing value handling

--- src/sf_datalake/preprocessing/post_join_processing.py
+++ src/sf_datalake/preprocessing/post_join_processing.py
@@ -15,9 +15,8 @@ USAGE
 import os
 import sys
 from os import path
-from typing import List
+from typing import Dict, List
 
-import pyspark.sql.functions as F
 from pyspark.ml import PipelineModel, Transformer
 
 # isort: off
@@ -50,39 +49,7 @@ input_ds = sf_datalake.io.load_data(
 )["input"]
 
 # Set every column name to lower case (if not already).
-siret_level_ds = input_ds.toDF(*(col.lower() for col in input_ds.columns))
-
-# Cast 'periode' to a "beginning of the month" date.
-siret_level_ds = siret_level_ds.withColumn(
-    "periode", F.to_date(F.date_trunc("month", F.col("periode")))
-)
-
-#####################
-# SIREN aggregation #
-#####################
-
-# Filter out public institutions and companies and aggregate at SIREN level
-siren_converter = sf_datalake.transform.SiretToSiren()
-aggregator = sf_datalake.transform.SirenAggregator(
-    grouping_cols=configuration.preprocessing.identifiers,
-    aggregation_map=configuration.preprocessing.siren_aggregation,
-    no_aggregation=[],
-)
-siren_level_ds = (
-    PipelineModel(stages=[siren_converter, aggregator])
-    .fit(siret_level_ds)
-    .select(
-        [
-            "periode",
-            "siren",
-            "apart_heures_consommees",
-            "cotisation",
-            "effectif",
-            "montant_part_ouvriere",
-            "montant_part_patronale",
-        ]
-    )
-)
+df = input_ds.toDF(*(col.lower() for col in input_ds.columns))
 
 #####################
 # Time Computations #
@@ -108,32 +75,37 @@ for feature, n_months in configuration.preprocessing.time_aggregation["mean"].it
         sf_datalake.transform.MovingAverage(inputCol=feature, n_months=n_months)
     )
 
-time_agg_ds = PipelineModel(stages=time_computations).transform(siren_level_ds)
+##########################
+# Missing Value Handling #
+##########################
 
-#######################
-# Feature engineering #
-#######################
+missing_values_handling_steps = []
+if configuration.preprocessing.fill_default_values:
+    missing_values_handling_steps.append(
+        sf_datalake.transform.MissingValuesHandler(
+            inputCols=list(configuration.preprocessing.fill_default_values),
+            value=configuration.preprocessing.fill_default_values,
+        ),
+    )
+if configuration.preprocessing.fill_imputation_strategy:
+    imputation_strategy_features: Dict[str, List[str]] = {}
+    for (
+        feature,
+        strategy,
+    ) in configuration.preprocessing.fill_imputation_strategy.items():
+        imputation_strategy_features.setdefault(strategy, []).append(feature)
 
-# Add "debt / contribution"
-feat_eng_ds = time_agg_ds.withColumn(
-    "dette_sur_cotisation_lissée",
-    (time_agg_ds["montant_part_ouvriere"] + time_agg_ds["montant_part_patronale"])
-    / time_agg_ds["cotisation_mean12m"],
-)
-# Moving average
-feat_eng_ds = sf_datalake.transform.MovingAverage(
-    inputCol="dette_sur_cotisation_lissée", n_months=12
-).transform(feat_eng_ds)
-# Average debt / workforce
-feat_eng_ds = feat_eng_ds.withColumn(
-    "dette_par_effectif",
-    (feat_eng_ds["montant_part_ouvriere"] + feat_eng_ds["montant_part_patronale"])
-    / feat_eng_ds["effectif"],
-)
-output_ds = sf_datalake.transform.DiffOperator(
-    inputCol="dette_par_effectif",
-    n_months=3,
-).transform(feat_eng_ds)
+    missing_values_handling_steps.extend(
+        sf_datalake.transform.MissingValuesHandler(
+            inputCols=features,
+            stat_strategy=strategy,
+        )
+        for strategy, features in imputation_strategy_features.items()
+    )
+
+output_ds = PipelineModel(
+    stages=time_computations + missing_values_handling_steps
+).transform(df)
 
 
 sf_datalake.io.write_data(output_ds, args.output, args.output_format)
