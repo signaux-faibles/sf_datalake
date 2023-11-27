cd2525a46fa663b286c454c35f8a05f77b6cb746
Hugo Simon <hsimon01@M093-1846016>
2023-11-27 09:25:33 +0100

    add example generator & make modification according to review

--- tests/test_model_selection.py
+++ tests/test_model_selection.py
@@ -1,9 +1,57 @@
 import datetime as dt
+import random
 
 import pytest
 from pyspark.sql import types as T
 
 from sf_datalake.model_selection import train_test_split
+
+
+# Function to generate example
+def generate_siren():
+    return "".join(str(random.randint(0, 9)) for _ in range(9))
+
+
+def generate_period():
+    l = [30 * i for i in range(2)]
+    n_days = l[random.randint(0, len(l) - 1)]
+    return dt.date(random.randint(2014, 2023), random.randint(1, 12), 1)
+
+
+def generate_value_integer():
+    return random.randint(0, 100)
+
+
+def generate_category():
+    return "".join(str(random.randint(0, 9)) for _ in range(3))
+
+
+def generate_value_double():
+    return random.random()
+
+
+def generate_label():
+    return random.randint(0, 1)
+
+
+def generate_data(n_siren, n_lines_by_siren, add_noise=False):
+    res = []
+    for _ in range(n_siren):
+        if add_noise:
+            n_lines_by_siren += random.randint(-1, 1)
+        s = generate_siren()
+        for _ in range(n_lines_by_siren):
+            res.append(
+                (
+                    s,
+                    generate_period(),
+                    generate_value_integer(),
+                    generate_value_double(),
+                    generate_category(),
+                    generate_label(),
+                )
+            )
+    return res
 
 
 @pytest.fixture(scope="class")
@@ -20,19 +68,7 @@ def split_dataset(spark):
     )
 
     df = spark.createDataFrame(
-        [
-            ("361277009", dt.date(2017, 11, 1), 81, 0.462595507496679, "755", 1),
-            ("361277009", dt.date(2016, 3, 1), 84, 0.5710125631116676, "304", 1),
-            ("779681095", dt.date(2014, 5, 1), 99, 0.6288750846033718, "764", 0),
-            ("779681095", dt.date(2022, 9, 1), 51, 0.22018893875685264, "144", 1),
-            ("166591555", dt.date(2021, 10, 1), 38, 0.4519904690474965, "369", 0),
-            ("166591555", dt.date(2014, 10, 1), 82, 0.246999907052183, "613", 1),
-            ("373921064", dt.date(2020, 7, 1), 10, 0.3331701759218788, "273", 1),
-            ("373921064", dt.date(2020, 4, 1), 13, 0.044001110200020954, "873", 1),
-            ("373921064", dt.date(2018, 1, 1), 66, 0.9032250349069598, "036", 0),
-            ("254020538", dt.date(2017, 8, 1), 8, 0.6128784436233967, "347", 0),
-            ("254020538", dt.date(2021, 9, 1), 49, 0.23821780577087037, "301", 0),
-        ],
+        generate_data(n_siren=1000, n_lines_by_siren=3, add_noise=True),
         schema,
     )
     return df
@@ -54,15 +90,18 @@ class TestTrainTestSplit:
         ), f"Intersection found: {siren_intersection.show()}"
 
     def test_split_dataset_train_test_size(self, split_dataset):
-        tolerance = 0.03
+        tolerance = 0.01
+        train_size = 0.8
+        test_size = 1 - train_size
+        seed = random.randint(0, 1000)
         train_data, test_data = train_test_split(
             df=split_dataset,
-            random_seed=42,
-            train_size=0.8,
-            test_size=0.2,
+            random_seed=seed,
+            train_size=train_size,
+            test_size=test_size,
             group_col="siren",
         )
         n_samples = split_dataset.count()
-        assert (test_data.count() / n_samples > 0.2 - tolerance) & (
-            test_data.count() / n_samples < 0.2 + tolerance
+        assert (test_data.count() / n_samples > test_size - tolerance) & (
+            test_data.count() / n_samples < test_size + tolerance
         )
