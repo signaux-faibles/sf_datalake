79ce52fbe4d6061f2c9bd4f5ac7bb2a57b80d790
Raphaël Ventura <raphael.ventura@beta.gouv.fr>
2023-12-07 16:57:53 +0100

    Switch to new schema, trunc dates, clip FPI values.

--- src/sf_datalake/preprocessing/altares_preprocessing.py
+++ src/sf_datalake/preprocessing/altares_preprocessing.py
@@ -1,24 +1,25 @@
-"""Altares historicized data preprocessing.
+"""Altares data preprocessing.
 
 This script parses and extracts data from raw historicized files supplied by Altares. It
 will output a dataset containing the following information:
 - siren
-- periode (date, first day of each month where data is available)
+- période (date, first day of each month where data is available)
 - paydex
-- fpi30
-- fpi90
+- fpi_30
+- fpi_90
 
-It also computes time-aggregates required through configuration.
+
+The provided file may be a concatenation of different files that are not sampled evenly
+accross time, therefore we truncate dates to month start.
 
 """
 
 import os
 import sys
 from os import path
-from typing import List
 
 import pyspark.sql.functions as F
-from pyspark.ml import PipelineModel, Transformer
+import pyspark.sql.types as T
 
 # isort: off
 sys.path.append(path.join(os.getcwd(), "venv/lib/python3.6/"))
@@ -45,56 +46,41 @@ parser.add_argument(
 args = parser.parse_args()
 
 configuration = sf_datalake.configuration.ConfigurationHelper(args.configuration)
-df = spark.read.csv(
-    args.input,
-    sep=";",
-    inferSchema=False,
-    header=True,
+
+paydex_schema = T.StructType(
+    [
+        T.StructField("siren", T.StringType(), False),
+        T.StructField("état_organisation", T.StringType(), True),
+        T.StructField("code_paydex", T.IntegerType(), True),
+        T.StructField("paydex", T.FloatType(), True),
+        T.StructField("n_fournisseurs", T.IntegerType(), True),
+        T.StructField("encours_étudiés", T.FloatType(), True),
+        T.StructField("fpi_30", T.FloatType(), True),
+        T.StructField("fpi_90", T.FloatType(), True),
+        T.StructField("période", T.StringType(), False),
+    ]
 )
 
-## Extraction
-df = df.withColumn(
-    "periode",
-    F.to_date(F.date_trunc("month", F.to_date(F.col("DATE_VALEUR"), "dd/MM/yyyy"))),
-)
-# We have to extract from a dumb string format.
-df = df.withColumn(
-    "paydex",
-    F.regexp_extract(
-        F.regexp_replace(F.col("PAYDEX"), r"(^Pas de retard)", "0"), r"(^\d+)", 1
-    ).cast("float"),
-)
-df = df.withColumn("fpi_30", F.col("NOTE100_ALERTEUR_PLUS_30").cast("float"))
-df = df.withColumn("fpi_90", F.col("NOTE100_ALERTEUR_PLUS_90_JOURS").cast("float"))
-df = df.withColumnRenamed("SIREN", "siren")
+df = spark.read.csv(args.input, sep=",", header=True, schema=paydex_schema)
 
-## Pre-processing
-time_computations: List[Transformer] = []
+## Pre-processing and export
+df = df.withColumn("période", F.trunc(format="month", date="période"))
+df = df.withColumn("fpi_30", sf_datalake.utils.clip("fpi_30", lower=0, upper=100) / 100)
+df = df.withColumn("fpi_90", sf_datalake.utils.clip("fpi_90", lower=0, upper=100) / 100)
 
-# pylint:disable=unsubscriptable-object
-for feature, n_months in configuration.preprocessing.time_aggregation["lag"].items():
-    if feature in df.columns:
-        time_computations.append(
-            sf_datalake.transform.LagOperator(inputCol=feature, n_months=n_months)
-        )
-for feature, n_months in configuration.preprocessing.time_aggregation["diff"].items():
-    if feature in df.columns:
-        time_computations.append(
-            sf_datalake.transform.DiffOperator(inputCol=feature, n_months=n_months)
-        )
-for feature, n_months in configuration.preprocessing.time_aggregation["mean"].items():
-    if feature in df.columns:
-        time_computations.append(
-            sf_datalake.transform.MovingAverage(inputCol=feature, n_months=n_months)
-        )
-df = PipelineModel(time_computations).transform(df)
-
-selected_cols = []
-for sel_col in ["siren", "periode", "paydex", "fpi_30", "fpi_90"]:
-    selected_cols.extend(df_col for df_col in df.columns if df_col.startswith(sel_col))
 
 sf_datalake.io.write_data(
-    df.select(selected_cols),
+    df.select(
+        [
+            "siren",
+            "période",
+            "paydex",
+            "fpi_30",
+            "fpi_90",
+            "n_fournisseurs",
+            "encours_étudiés",
+        ]
+    ),
     args.output,
     args.output_format,
 )
