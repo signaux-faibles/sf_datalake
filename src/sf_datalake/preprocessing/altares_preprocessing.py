3741ea53b7d301f594ce2115317a26aa8fe3a7bc
Hugo SIMON <hsimon01@M093-1846016>
2023-12-13 09:22:08 +0100

    solve merge conflict on altares_preprocessing

--- src/sf_datalake/preprocessing/altares_preprocessing.py
+++ src/sf_datalake/preprocessing/altares_preprocessing.py
@@ -1,14 +1,17 @@
-"""Altares historicized data preprocessing.
+"""Altares data preprocessing.
 
 This script parses and extracts data from raw historicized files supplied by Altares. It
 will output a dataset containing the following information:
 - siren
 - période (date, first day of each month where data is available)
 - paydex
-- fpi30
-- fpi90
+- fpi_30
+- fpi_90
 
-It also computes time-aggregates required through configuration.
+
+The input file may be a concatenation of different files that are not sampled evenly
+across time. In case there are multiple rows for a given (SIREN, month) couple, we only
+keep the most recent data and truncate date to the first day of month.
 
 """
 
@@ -17,6 +20,7 @@ import sys
 from os import path
 
 import pyspark.sql.functions as F
+import pyspark.sql.types as T
 
 # isort: off
 sys.path.append(path.join(os.getcwd(), "venv/lib/python3.6/"))
@@ -33,49 +37,46 @@ spark = sf_datalake.utils.get_spark_session()
 parser = sf_datalake.io.data_path_parser()
 parser.description = "Extract and pre-process Altares data."
 parser.add_argument(
-    "--configuration",
-    help="Configuration file containing required time-computations.",
-    required=True,
-)
-parser.add_argument(
     "--output_format", default="orc", help="Output dataset file format."
 )
 args = parser.parse_args()
 
-configuration = sf_datalake.configuration.ConfigurationHelper(args.configuration)
-df = spark.read.csv(
-    args.input,
-    sep=";",
-    inferSchema=False,
-    header=True,
+paydex_schema = T.StructType(
+    [
+        T.StructField("siren", T.StringType(), False),
+        T.StructField("état_organisation", T.StringType(), True),
+        T.StructField("code_paydex", T.IntegerType(), True),
+        T.StructField("paydex", T.FloatType(), True),
+        T.StructField("n_fournisseurs", T.IntegerType(), True),
+        T.StructField("encours_étudiés", T.FloatType(), True),
+        T.StructField("fpi_30", T.FloatType(), True),
+        T.StructField("fpi_90", T.FloatType(), True),
+        T.StructField("date", T.DateType(), False),
+    ]
 )
 
-## Extraction
-df = df.withColumn(
-    "période",
-    F.to_date(F.date_trunc("month", F.to_date(F.col("DATE_VALEUR"), "dd/MM/yyyy"))),
-)
-# We have to extract from a dumb string format.
-df = df.withColumn(
+df = spark.read.csv(args.input, sep=",", header=True, schema=paydex_schema)
+num_cols = [
     "paydex",
-    F.regexp_extract(
-        F.regexp_replace(F.col("PAYDEX"), r"(^Pas de retard)", "0"), r"(^\d+)", 1
-    ).cast("float"),
-)
-df = df.withColumn("fpi_30", F.col("NOTE100_ALERTEUR_PLUS_30").cast("float"))
-df = df.withColumn("fpi_90", F.col("NOTE100_ALERTEUR_PLUS_90_JOURS").cast("float"))
-df = df.withColumnRenamed("SIREN", "siren")
+    "fpi_30",
+    "fpi_90",
+    "encours_étudiés",
+    "n_fournisseurs",
+]
 
-selected_cols = ["siren", "période", "paydex", "fpi_30", "fpi_90"]
+## Pre-processing and export
 
-# Handle missing values and export
-mvh = sf_datalake.transform.MissingValuesHandler(
-    inputCols=["paydex", "fpi_30", "fpi_90"],
-    value=configuration.preprocessing.fill_default_values,
+# Clip and normalize FPIs to the [0, 1] range
+df = df.withColumn("fpi_30", sf_datalake.utils.clip("fpi_30", lower=0, upper=100) / 100)
+df = df.withColumn("fpi_90", sf_datalake.utils.clip("fpi_90", lower=0, upper=100) / 100)
+
+# There may be multiple data for a given month, in which case we only keep last values
+df = df.groupBy(["siren", F.trunc(format="month", date="date").alias("période")]).agg(
+    *(F.last(col).alias(col) for col in num_cols)
 )
 
 sf_datalake.io.write_data(
-    mvh.transform(df.select(selected_cols)),
+    df,
     args.output,
     args.output_format,
 )
