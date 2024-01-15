926b58dec49d66c5d31635be071372b1d9bbc1e2
Hugo Simon <hsimon01@M093-2013079>
2024-01-15 16:58:13 +0100

    modify main + fix problem with dette into config

--- src/sf_datalake/__main__.py
+++ src/sf_datalake/__main__.py
@@ -67,29 +67,11 @@ path_group.add_argument(
     """,
 )
 parser.add_argument(
-    "--train_dates",
-    type=str,
-    nargs=2,
-    help="The training set start and end dates (YYYY-MM-DD format).",
-)
-parser.add_argument(
-    "--prediction_date",
-    type=str,
-    help="The date over which prediction should be made (YYYY-MM-DD format).",
-)
-parser.add_argument(
     "--sample_ratio",
     type=float,
     help="Loaded data sample size as a fraction of its full size.",
 )
-parser.add_argument(
-    "--drop_missing_values",
-    action="store_true",
-    help="""
-    If specified, missing values will be dropped instead of filling data with default
-    values.
-    """,
-)
+
 parser.add_argument(
     "--seed",
     dest="random_seed",
@@ -150,6 +132,10 @@ if any(
     )
 
 ## Pre-processing pipeline
+# Remove uncomplete cols
+raw_dataset = raw_dataset.drop(
+    *["encours_étudiés", "n_fournisseurs", "code_naf", "région"]
+)
 
 filter_steps = []
 if with_paydex:
